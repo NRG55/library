ca6163b1b1add5dc84c08ca51739c8d4fc21c972
NRG55 <maloj_1980@yahoo.fr>
2024-03-26 02:38:11 +0000

    The app is changed - instead of generating divs, it generates a table with a user input details. Each row of the table has two buttons: the first button lets a user to change a status of the book (read or not read) and the second one deletes a row, all functions for that are done too.

--- app.js
+++ app.js
@@ -1,98 +1,102 @@
-const myBooks = document.querySelector('.books-container');
+const table = document.querySelector('table');
+const tbody = document.querySelector('.books-table');
 const btnNewBook = document.querySelector('#btn-new-book');
+
 const modalForm = document.querySelector('.modal');
 const btnSubmitForm = document.querySelector('.btn-submit-form');
-const myLibrary = [];
+const checkboxRead = document.querySelector('#checkbox-read');
+let myLibrary = [];
 let myBook = {};
 
-function Book(title, author, pages, read) { 
+function Book(title, author, pages, read) {
+    this.id = `book-${++Book.id}`; 
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = Boolean(read);
+    this.read = read;
 }
+
+Book.id = 0;
 
 function addBookToLibrary() {    
     let title = document.querySelector('#title').value;
     let author = document.querySelector('#author').value;
     let pages = document.querySelector('#pages').value;
-    let read = document.querySelector('#read').value; 
+    let read = document.querySelector('#checkbox-read').value;
 
-    myBook = new Book(title, author, pages, read);
-    myLibrary.push(myBook);
-    console.log(myBook);
-    console.log(myLibrary); 
+    myBook = new Book(title, author, pages, read);   
+    myLibrary.push(myBook);   
     return myBook;     
 }
 
-function createBookElement(el, info, className) {
-    if (el === 'input') {
-        const read = document.createElement('div');
-        const label = document.createElement('label');
-        const input = document.createElement('input');        
-        read.setAttribute('class', className);       
-        input.type = 'checkbox';       
-        label.textContent = info;
-        console.log(label);
-        read.appendChild(label);
-        read.appendChild(input);
-        read.addEventListener('click', (e) => {
-            if (e.target.checked) {
-                input.setAttribute('class', 'read-checked');
-                myBook.read = true;                
+function createBookHTML(book) {
+    return `
+        <tr>
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.pages}</td>
+            <td><button id=read-${book.id}>${book.read}</button></td>
+            <td><button id=${book.id}>x</button></td>
+        </tr>  
+    `   
+}
+
+function render() {
+    const html = myLibrary.map(createBookHTML).join('');
+    // console.log(html);
+    tbody.innerHTML = html;
+    // console.log(myLibrary)
+    for (let i = 0; i < myLibrary.length; i++) {
+        const btnDelete = document.getElementById(`${myLibrary[i].id}`);
+        const btnRead = document.getElementById(`read-${myLibrary[i].id}`);
+      
+        btnDelete.addEventListener('click', () => {
+            deleteBook(myLibrary[i]);
+        });
+        
+        btnRead.addEventListener('click', () => {
+            if (myLibrary[i].read === 'read') {
+                myLibrary[i].read = 'not read';
+                btnRead.textContent = 'not read';                
+                render();
+                console.log(myLibrary[i].read)
             } else {
-                input.setAttribute('class', 'read-unchecked');
-                myBook.read = false;             
-            }            
-        })        
-        return read;         
-        } else {
-            const bookElement = document.createElement(el);
-            bookElement.textContent = info;
-            console.log(info)
-            bookElement.setAttribute('class', className);
-            return bookElement;
-        }    
+                myLibrary[i].read = 'read';
+                btnRead.textContent = 'read';               
+                render();
+                console.log(myLibrary[i].read)
+            }           
+        });        
+    }
 }
 
-function createBookUnit(book, index) {
-    const bookUnit = document.createElement('div');  
-
-    bookUnit.setAttribute('id', index);
-    bookUnit.setAttribute('class', 'book book-div');
-    bookUnit.appendChild(createBookElement('div', 'Title:' + '\r\n' + `${book.title}`, 'book book-title'));
-    bookUnit.appendChild(createBookElement('div', 'Author:' + '\r\n' + `${book.author}`, 'book book-author'));
-    bookUnit.appendChild(createBookElement('div', 'Pages:' + '\r\n' + `${book.pages}`, 'book book-pages'));
-    bookUnit.appendChild(createBookElement('input', `Read: ${book.read}`, 'book read'));
-    bookUnit.appendChild(createBookElement('button', 'X', 'btn-delete'));
-
-    const btnDelete = bookUnit.querySelector('.btn-delete');
-    btnDelete.addEventListener('click', () => {
-        myLibrary.splice(index, 1);
-        updateLibrary();
-    }); 
-
-    myBooks.insertAdjacentElement('afterbegin', bookUnit);
-}    
-
-function updateLibrary() {
-    myBooks.textContent = '';
-    myLibrary.map((book, index) => {
-        createBookUnit(book, index)
-    })
+function deleteBook(book) {
+    myLibrary = myLibrary.filter(e => e.id !== book.id)
+    render();
 }
 
-btnNewBook.addEventListener('click', function() {   
-    modalForm.style.display = 'block';
+btnNewBook.addEventListener('click', () => {   
+    // modalForm.style.display = 'block';
+    modalForm.showModal();
 })
 
-btnSubmitForm.addEventListener('click', function() {   
-    modalForm.style.display = 'none';
+checkboxRead.addEventListener('click', () =>{
+    if (checkboxRead.checked) {
+        myLibrary.read = 'read';
+        checkboxRead.value = 'read';        
+    } else {
+        myLibrary.read = 'not read';
+        checkboxRead.value = 'not read';
+    }
+})
+
+btnSubmitForm.addEventListener('click', () => {   
+    // modalForm.style.display = 'none';    
+    modalForm.close();
 })
 
 modalForm.addEventListener('submit', function(event) {
-    event.preventDefault();
+    event.preventDefault();    
     addBookToLibrary();
-    updateLibrary();  
-    console.log(myBook.title);      
+    render();    
 })
